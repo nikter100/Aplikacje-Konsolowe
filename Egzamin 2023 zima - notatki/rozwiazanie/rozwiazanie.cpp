2c0f4246fbbcfbbeca8f90d9e8b3b4e9752e1f9d
Jakub Spendel <jspendel45@gmail.com>
2025-06-11 01:23:24 +0200

    Opracowanie akrusza z zimy 2023 roku polegającego na notatkach przy pomocy obiektów oraz konstruktorów

---
+++ Egzamin 2023 zima - notatki/rozwiazanie/rozwiazanie.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Notatka
+{
+public:
+    Notatka(string, string);
+
+    void wyswietl();
+    void wypiszWszystko();
+ 
+protected:
+    string tytul, tresc;
+
+private:
+    static int licznik;
+    int index;
+};
+
+int Notatka::licznik = 0;
+
+Notatka::Notatka(string tytulNot, string trescNot)
+{
+    tytul = tytulNot;
+    tresc = trescNot;
+    licznik++;
+    index = licznik;
+}
+
+void Notatka::wyswietl(){
+    cout << "Tytul: " << tytul << endl;
+    cout << "Tresc: " << tresc << endl;
+}
+
+void Notatka::wypiszWszystko() {
+    cout << tytul << "; " << tresc << "; " << licznik << "; " << index<<endl;
+}
+
+
+int main()
+{
+    Notatka n1("Zakupy", "Mleko, Jajka, Maslo");
+    n1.wyswietl();
+    n1.wypiszWszystko();
+    Notatka n2("Do zrobienia", "Nauczyc sie c++");
+    n2.wyswietl();
+    n2.wypiszWszystko();
+}
